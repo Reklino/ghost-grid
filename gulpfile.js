dd04157fbd950a198a8fbd1fb53ac4a3e6186b50
Reklino <yakub89@gmail.com>
2015-05-05 16:58:19 -0500

    type changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,32 +4,41 @@ var autoprefixer = require('gulp-autoprefixer');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
 
 
 gulp.task('sass', function() {
-	return gulp.src('demo/sass/*.scss')
-		.pipe(sass({
+    return gulp.src('demo/sass/*.scss')
+        .pipe(sass({
             includePaths: [
                 'node_modules/susy/sass' //required for sass
             ]
         }))
-		.pipe(autoprefixer())
-		.pipe(gulp.dest('demo/stylesheets'));
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('demo/stylesheets'))
+        .pipe(reload({stream: true}));
 
 });
 
 gulp.task('js', function() {
-	return gulp.src('dist/ghost-grid.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'))
-		.pipe(uglify())
-		.pipe(rename('ghost-grid.min.js'))
-    	.pipe(gulp.dest('dist'));
+    return gulp.src('dist/ghost-grid.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(uglify())
+        .pipe(rename('ghost-grid.min.js'))
+        .pipe(gulp.dest('dist'));
 })
 
 gulp.task('watch', function() {
-  gulp.watch('demo/sass/*.scss', ['sass'])
-  gulp.watch('dist/*.js', ['js'])
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('demo/sass/*.scss', ['sass'])
+    gulp.watch('dist/*.js', ['js']).on('change', reload);
+    gulp.watch("demo/*.html").on('change', reload);
 })
 
 gulp.task('default', ['sass', 'js', 'watch']);
